d3e9371e86f66bb15ee01c4ee1394ee132dfbe2c
Bailey OConnor <bcoconnor4@wisc.edu>
2024-02-25 18:32:46 -0600

    Exercise 8. Source: https://www.youtube.com/watch?v=aywbIWIccKk

--- flag.py
+++ flag.py
@@ -1,20 +1,55 @@
 from turtle import *
+speed(0)
+setup(800,500)
 
 ### Functions for drawing
 # Modify the code in this function
 
-def bar(bar_color):
-    pass
+penup()
+goto(-400,250)
+pendown()
+
+### Main code that gets run, to draw the german flag
+#Black Rectangle
+color("black")
+begin_fill()
+forward(800)
+right(90)
+forward(167)
+right(90)
+forward(800)
+end_fill()
+
+left(90)
+
+#Red Rectangle
+color("red")
+begin_fill()
+forward(167)
+left(90)
+forward(800)
+left(90)
+forward(167)
+end_fill()
+
+
+left(180)
+penup()
+forward(167)
+pendown()
+
+#Yellow Rectangle
+color("yellow")
+begin_fill()
+forward(167)
+right(90)
+forward (800)
+right(90)
+forward(167)
+end_fill()
 
 ### Main code that gets run, to draw the german flag
 # Do not modify code below this line for Exercise 7.  It is OK to add code below this for Exercise 8
-
-bar("black")
-right(90); forward(50); left(90)
-bar("red")
-right(90); forward(50); left(90)
-bar("yellow")
-
 done()
 
     
